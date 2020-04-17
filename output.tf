78195314cd1e3cd1342dafe2d1875f552832f75c
Bharat Kunwar <brtknr@bath.edu>
2020-04-17 11:48:35 +0100

    Combined ansible inventory

--- output.tf
+++ output.tf
@@ -16,20 +16,18 @@ output "ips_monasca" {
   ))}"
 }
 
-output "inventory_all_in_one" {
-  value = "${
-    join("", formatlist(
-      "\n    %s ansible_host=%s ansible_user=lab",
-      packet_device.all_in_one.*.hostname,
-      packet_device.all_in_one.*.access_public_ipv4
-  ))}"
-}
-
-output "inventory_monasca" {
-  value = "${
-    join("", formatlist(
-      "\n    %s ansible_host=%s ansible_user=lab",
-      packet_device.monasca.*.hostname,
-      packet_device.monasca.*.access_public_ipv4
-  ))}"
+output "ansible_inventory" {
+  value = <<EOT
+.
+    [all_in_one]
+${join("\n",
+       formatlist("    %s ansible_host=%s ansible_user=lab",
+                   packet_device.all_in_one.*.hostname,
+                   packet_device.all_in_one.*.access_public_ipv4))}
+    [monasca]
+${join("\n",
+       formatlist("    %s ansible_host=%s ansible_user=lab",
+                  packet_device.monasca.*.hostname,
+                  packet_device.monasca.*.access_public_ipv4))}
+EOT
 }
