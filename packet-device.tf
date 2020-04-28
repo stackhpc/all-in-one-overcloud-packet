99c4a42ae17557d63638a5527a9871a8b1bae9c2
Pierre Riteau <pierre@stackhpc.com>
2020-04-28 11:07:14 +0200

    Fix Terraform deprecation warnings

--- packet-device.tf
+++ packet-device.tf
@@ -1,32 +1,32 @@
 provider "packet" {
-  auth_token = "${var.packet_auth_token}"
+  auth_token = var.packet_auth_token
 }
 
 resource "packet_ssh_key" "default" {
   name       = "default"
-  public_key = "${tls_private_key.default.public_key_openssh}"
+  public_key = tls_private_key.default.public_key_openssh
 }
 
 resource "packet_device" "all_in_one" {
 
-  depends_on = ["packet_ssh_key.default"]
+  depends_on = [packet_ssh_key.default]
 
-  count            = "${var.lab_count}"
-  hostname         = "${format("%s-all-in-one-%02d", var.deploy_prefix, count.index)}"
-  operating_system = "${var.operating_system}"
-  plan             = "${var.plan}"
+  count            = var.lab_count
+  hostname         = format("%s-all-in-one-%02d", var.deploy_prefix, count.index)
+  operating_system = var.operating_system
+  plan             = var.plan
 
-  facilities    = ["${var.packet_facility}"]
-  project_id    = "${var.packet_project_id}"
+  facilities    = [var.packet_facility]
+  project_id    = var.packet_project_id
   billing_cycle = "hourly"
 }
 
 resource "packet_device" "monasca" {
 
-  depends_on = ["packet_ssh_key.default"]
+  depends_on = [packet_ssh_key.default]
 
   count            = var.lab_count
-  hostname         = "${format("%s-monasca-%02d", var.deploy_prefix, count.index)}"
+  hostname         = format("%s-monasca-%02d", var.deploy_prefix, count.index)
   operating_system = var.operating_system
   plan             = var.plan
 
