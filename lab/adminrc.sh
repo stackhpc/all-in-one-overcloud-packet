3673c304061763f6f9a9812d53d2e1a1c6d6c70b
Bharat Kunwar <brtknr@bath.edu>
2020-05-01 16:49:32 +0100

    Create a new virtualenv for using OpenStack

--- lab/adminrc.sh
+++ lab/adminrc.sh
@@ -1,3 +1,4 @@
-source ~/kayobe-venv/bin/activate
-pip install -q python-openstackclient python-magnumclient python-heatclient python-monascaclient
+[[ -d ~/venv ]] || virtualenv ~/venv
+source ~/venv/bin/activate
+pip install -qr ~/requirements.txt
 source ~/kayobe/config/src/kayobe-config/etc/kolla/public-openrc.sh
