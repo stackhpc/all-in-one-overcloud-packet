50b715bf30839f82c31105b292ec27846cc1369d
Bharat Kunwar <bharat@stackhpc.com>
2020-05-04 14:47:03 +0000

    Use admin-openrc instead of public-openrc

--- lab/adminrc.sh
+++ lab/adminrc.sh
@@ -3,6 +3,6 @@ pushd `dirname ${BASH_SOURCE[0]}`
 [[ -d venv ]] || virtualenv venv
 source venv/bin/activate
 pip install -qr requirements.txt
-source kayobe/config/src/kayobe-config/etc/kolla/public-openrc.sh
+source kayobe/config/src/kayobe-config/etc/kolla/admin-openrc.sh
 
 popd
