7b05360cd037a94a028f5e9893ad9796b9b7b4ca
Bharat Kunwar <brtknr@bath.edu>
2020-05-05 11:04:24 +0100

    Remove reference to

--- lab/all-in-one.sh
+++ lab/all-in-one.sh
@@ -25,12 +25,12 @@ openstack user create $USERNAME --password $OS_PASSWORD --or-show
 openstack role add --user $USERNAME --project $PROJECT_NAME member
 
 # Set default gateway on subnet so that traffic from vms can reach outside world
-source $LAB_DIR/adminrc.sh
+source adminrc.sh
 openstack network create public --external --provider-network-type=flat --provider-physical-network physnet1 --share
 openstack subnet create public-subnet --ip-version=4 --gateway=$PUBLIC_NETWORK_GW_IP --network public --allocation-pool start=172.24.4.2,end=172.24.4.254 --subnet-range 172.24.4.0/24
 
 # Keypair for lab user
-source $LAB_DIR/labrc.sh
+source labrc.sh
 openstack keypair create default --public-key ~/.ssh/id_rsa.pub
 
 popd
