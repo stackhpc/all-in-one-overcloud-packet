2c8476d254e636360e70cf856e6920e22d9051ac
Bharat Kunwar <brtknr@bath.edu>
2020-05-05 09:44:44 +0100

    Assume git is available

--- lab/stack.sh
+++ lab/stack.sh
@@ -5,8 +5,8 @@ set -x
 # Apply network configuration (may need to reapply this after a reboot)
 ./configure-network.sh ${CONTROLLER_IP:-192.168.33.3} ${PUBLIC_NETWORK_GW_IP}
 
-# Install git and disable selinux
-sudo dnf install -y git selinux-policy-targeted
+# Disable selinux
+sudo dnf install -y selinux-policy-targeted
 sudo sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
 sudo setenforce 0
 
