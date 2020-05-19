7162a65b37eb8a0c6f05c832750c12233c95c4b4
Bharat Kunwar <brtknr@bath.edu>
2020-05-19 10:58:46 +0100

    Improve output

--- output.tf
+++ output.tf
@@ -1,21 +1,26 @@
 output "ips_all_in_one" {
   value = "${
     join("", formatlist(
-      "\n    ssh -o PreferredAuthentications=password lab@%s #password: %s",
+      "\n    %s (password: %s) ssh lab@%s",
+      packet_device.all_in_one.*.hostname,
+      packet_device.all_in_one.*.id,
       packet_device.all_in_one.*.access_public_ipv4,
-      packet_device.all_in_one.*.id
   ))}"
 }
 
 output "ips_monasca" {
   value = "${local.monasca_lab_count > 0 ?
     join("", formatlist(
-      "\n    ssh -o PreferredAuthentications=password lab@%s #password: %s",
+      "\n    %s (password: %s) ssh lab@%s",
+      packet_device.monasca.*.hostname,
+      packet_device.all_in_one.*.id,
       packet_device.monasca.*.access_public_ipv4,
-      packet_device.all_in_one.*.id
   )) : ""}"
 }
 
+output "ips_ssh_arg" {
+  value = "You may need -o PreferredAuthentications=password argument."
+}
 output "inventory_all_in_one" {
   value = "${
     join("",
